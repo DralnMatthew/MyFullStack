b7319251931c803b2e7970dba932aa522dda8ff9
matt <zhanghu.zhao@outlook.com>
2022-08-11 14:50:40 +0300

    Revert "Revert "part7""
    This reverts commit 6dd2ef43333fefc55c104ccf50ebdb48c9ab76c7.

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  const fetchData = async (name) => {
+    if (name === '') {
+      setCountry(null)
+      return
+    }
+    try {
+      const response = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      if (response.status === 404) {
+        setCountry(null)
+        return
+      }
+      setCountry(response.data[0])
+    } catch (error) {
+      setCountry(null)
+    }
+  }
+
+  useEffect( () => {
+    fetchData(name).then(r => r)
+  }, [name])
+  // console.log(country)
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return <div>not found...</div>
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common}</h3>
+      <div>population {country.population}</div> 
+      <div>capital {country.capital}</div>
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+    </div>
+  )  
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
